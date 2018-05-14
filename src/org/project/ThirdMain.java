e7311e964c21b1d5c21225c2a2ec2c789d64c791
sasmitapradhan01 <sasmitapradhan01@gmail.com>
2018-05-13 23:13:32 -0400

    Ok

--- src/org/project/ThirdMain.java
+++ src/org/project/ThirdMain.java
@@ -5,5 +5,6 @@ public class ThirdMain {
     public static void main(String[] args) {
 	// write your code here
         System.out.println("wow another commit");
+        System.out.println("ok");
     }
 }
